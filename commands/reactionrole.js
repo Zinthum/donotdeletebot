6eddafc4d4edf1eec794adde17837f1a84fde10b
Zinthum <100621850+Zinthum@users.noreply.github.com>
2022-03-02 18:01:51 +0200

    reactionrole

--- commands/reactionrole.js
+++ commands/reactionrole.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: 'reactionrole',
     description: "Sets up a reaction role message!",
-    async execute(message, args, Discord, client){
+    async execute(message, args, Discord, client) {
         const channel = '<#877205506383573052>';
         const GiveawayRole = message.guild.roles.cache.find(role => role.name === "Giveaway");
         const PollRole = message.guild.roles.cache.find(role => role.name === "Poll");
@@ -19,5 +19,44 @@ module.exports = {
         let messageEmbed = await message.channel.send(embed);      
         messageEmbed.react(GiveawayEmoji);
         messageEmbed.react(PollEmoji);
+
+        client.on('messageReactionAdd', async (reaction, user) => {
+            if (reaction.message.partial) await reaction.message.fetch();
+            if (reaction.partial) await reaction.fetch();
+            if (user.bot) return;
+            if (!reaction.message.guild) return;
+
+            if (reaction.message.channel.id == channel) {
+                if (reaction.emoji.name === GiveawayEmoji) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.add(GiveawayRole);
+                }
+                if (reaction.emoji.name === PollEmoji) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.add(PollRole);
+                }
+            } else {
+                return;
+            }
+ 
+        });
+ 
+        client.on('messageReactionRemove', async (reaction, user) => {
+ 
+            if (reaction.message.partial) await reaction.message.fetch();
+            if (reaction.partial) await reaction.fetch();
+            if (user.bot) return;
+            if (!reaction.message.guild) return;
+ 
+            if (reaction.message.channel.id == channel) {
+                if (reaction.emoji.name === GiveawayEmoji) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.remove(GiveawayRole);
+                }
+                if (reaction.emoji.name === PollEmoji) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.remove(PollRole);
+                }
+            } else {
+                return;
+            }
+        });
     }
+
 }
--- main.js
+++ main.js
@@ -9,13 +9,13 @@ const fs = require('fs');
 client.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
-for (const file of commandFiles){
+for(const file of commandFiles){
     const command = require(`./commands/${file}`);
 
     client.commands.set(command.name, command);
 }
 
-client.on('ready', () => {
+client.once('ready', () => {
     console.log("Zinthum's Bot is online!");
 });
 
