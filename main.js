a39adcec45666687b60633e9f50c6cea5bf1307f
Zinthum <100621850+Zinthum@users.noreply.github.com>
2022-03-02 17:37:06 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,17 @@ const client = new Discord.Client({intents:["GUILDS","GUILDS_MESSAGES"]});
 
 const prefix = '.';
 
+const fs = require('fs');
+
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+
+    client.commands.set(command.name, command);
+}
+
 client.once('ready', () => {
     console.log("Zinthum's Bot is online!");
 });
