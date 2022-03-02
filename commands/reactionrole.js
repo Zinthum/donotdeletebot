8616ee31903c8df12cd72e8a74e4820551375ae8
Zinthum <100621850+Zinthum@users.noreply.github.com>
2022-03-03 00:12:54 +0200

    Update reactionrole.js

--- commands/reactionrole.js
+++ commands/reactionrole.js
@@ -1,62 +1,49 @@
 module.exports = {
     name: 'reactionrole',
     description: "Sets up a reaction role message!",
-    async execute(message, args, Discord, client){
-        const channel = '877205506383573052';
-        const GiveawayRole = message.guild.roles.cache.find(role => role.name === "Giveaway");
-        const PollRole = message.guild.roles.cache.find(role => role.name === "Poll");
-
-        const GiveawayEmoji = '🌈';
-        const PollEmoji = '🎲';
-
+    async execute(message, args, Discord, client) {
         let embed = new Discord.MessageEmbed()
-            .setColor('#e42643')
-            .setTitle('Choose a role to never miss an event!')
-            .setDescription('Choosing a role will allow you to never miss any events!\n\n'
-                + `${GiveawayEmoji} for Giveaway\n`
-                + `${PollEmoji} for Poll`);
+           .setColor('#e42643')
+           .setTitle('Choose a role to never miss an event!')
+           .setDescription('Choosing a role will allow you to never miss any events!\n\n' + `${GiveawayEmoji} for Giveaway\n` + `${PollEmoji} for Poll`);
+        message.delete();
 
-        let messageEmbed = await message.channel.send({ embeds: [embed] });      
-        messageEmbed.react(GiveawayEmoji);
-        messageEmbed.react(PollEmoji);
-
-        client.on('messageReactionAdd', async (reaction, user) => {
-            if (reaction.message.partial) await reaction.message.fetch();
-            if (reaction.partial) await reaction.fetch();
-            if (user.bot) return;
-            if (!reaction.message.guild) return;
-
-            if (reaction.message.id === "948696113353990195") {
-                if (reaction.emoji.name === GiveawayEmoji) {
-                await reaction.message.guild.members.cache.get(user.id).roles.add('877205752165568614');
-                }
-            }
-            if (reaction.message.id === "948696113353990195") {
-                if (reaction.emoji.name === PollEmoji) {
-                await reaction.message.guild.members.cache.get(user.id).roles.add('939600919748804698');
-                }
-            }
- 
-        });
- 
-        client.on('messageReactionRemove', async (reaction, user) => {
- 
-            if (reaction.message.partial) await reaction.message.fetch();
-            if (reaction.partial) await reaction.fetch();
-            if (user.bot) return;
-            if (!reaction.message.guild) return;
- 
-            if (reaction.message.id === "948696113353990195") {
-                if (reaction.emoji.name === GiveawayEmoji) {
-                await reaction.message.guild.members.cache.get(user.id).roles.remove('877205752165568614');
-                }
-            }
-            if (reaction.message.id === "948696113353990195") {
-                if (reaction.emoji.name === PollEmoji) {
-                await reaction.message.guild.members.cache.get(user.id).roles.remove('939600919748804698');
-                }
-            }
-        });
+        const msg = await message.channel.send({ embeds: [embed] }); 
+        await msg.react('🌈');
+        await msg.react('🎲');
     }
-
 }
+
+client.on("messageReactionAdd", async (reaction, member, guild, message) => {
+    if (reaction.message.partial) await reaction.message.fetch();
+  if (reaction.partial) await reaction.fetch();
+  if (member.bot) return;
+  if (!reaction.message.guild) return;
+  if (reaction.message.id === "id of bots message") {
+    if (reaction.emoji.name === "🌈") {
+      await reaction.message.guild.members.cache.get(member.id).roles.add("role ID");
+    }
+  }
+  if (reaction.message.id === "id of bots message") {
+    if (reaction.emoji.name === "🎲") {
+      await reaction.message.guild.members.cache.get(member.id).roles.add("role ID");
+    }
+  }
+});
+
+client.on("messageReactionRemove", async (reaction, user) => {
+    if (reaction.message.partial) await reaction.message.fetch();
+    if (reaction.partial) await reaction.fetch();
+    if (user.bot) return;
+    if (!reaction.message.guild) return;
+    if (reaction.message.id === "id of bots message") {
+      if (reaction.emoji.name === "🌈") {
+        await reaction.message.guild.members.cache.get(user.id).roles.remove("role id");
+      }
+    }
+    if (reaction.message.id === "id of bots message") {
+      if (reaction.emoji.name === "🎲") {
+        await reaction.message.guild.members.cache.get(user.id).roles.remove("role id");
+      }
+    } 
+  });
