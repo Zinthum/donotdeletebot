9df618fde10ceedb3ca62a7537e60bbd3afdda2c
Zinthum <100621850+Zinthum@users.noreply.github.com>
2022-03-03 16:00:04 +0200

    Update main.js

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ client.on('ready', () => {
     console.log("Zinthum's Bot is online!");
 });
 
-client.on('messageCreate', message =>{
+client.on('messageCreate', message => {
     if(!message.content.startsWith(prefix) || message.author.bot) return;
 
     const args = message.content.slice(prefix.length).split(/ + /);
