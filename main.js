b4326b689588bb2f4287f0a60fcec4650717629b
Zinthum <100621850+Zinthum@users.noreply.github.com>
2022-03-04 18:13:01 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 
-const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]});
+const client = new Discord.Client({intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES"]});
 
 const prefix = '.';
 
