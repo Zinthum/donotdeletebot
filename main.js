13de27ff21acfb4ede522ccc6d60cc44ba1c9ab1
Zinthum <100621850+Zinthum@users.noreply.github.com>
2022-03-11 10:57:20 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,6 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]});
 
-client.user.setStatus("LOL");
-
 const prefix = '.';
 
 const fs = require('fs');
